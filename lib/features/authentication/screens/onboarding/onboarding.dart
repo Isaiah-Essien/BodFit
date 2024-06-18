2b78ce4fb69332fd54564405334f31c3a1d6ca18
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-06-19 00:56:09 +0200

    Completed 3 onboaeding screens Front end

--- lib/features/authentication/screens/onboarding/onboarding.dart
+++ lib/features/authentication/screens/onboarding/onboarding.dart
@@ -1,10 +1,12 @@
+import 'package:bodFit_group5_summative/features/authentication/screens/onboarding/widgets/onboardig_next_button.dart';
+import 'package:bodFit_group5_summative/features/authentication/screens/onboarding/widgets/onboarding_dot_navigation.dart';
+import 'package:bodFit_group5_summative/features/authentication/screens/onboarding/widgets/onboarding_page.dart';
+import 'package:bodFit_group5_summative/features/authentication/screens/onboarding/widgets/onboarding_skip.dart';
 import 'package:bodFit_group5_summative/utils/constants/images_string.dart';
-import 'package:bodFit_group5_summative/utils/constants/sizes.dart';
 import 'package:bodFit_group5_summative/utils/constants/text_strings.dart';
-import 'package:bodFit_group5_summative/utils/device/device_utils.dart';
-import 'package:bodFit_group5_summative/utils/helpers/helpers_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
@@ -36,66 +38,13 @@ class OnBoardingScreen extends StatelessWidget {
           ),
 
           ///Skip Button
-          const OnBoardingSkip()
+          const OnBoardingSkip(),
 
           ///Dot Navigation SmoothPageIndicator
+          const OnBoardingDotNavigation(),
 
           ///Circular button
-        ],
-      ),
-    );
-  }
-}
-
-class OnBoardingSkip extends StatelessWidget {
-  const OnBoardingSkip({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-        top: MDeviceUtils.getAppBarHeight(),
-        right: MSizes.defaultSpace,
-        child: TextButton(
-          onPressed: () {},
-          child: const Text('Skip'),
-        ));
-  }
-}
-
-class OnBoardingPage extends StatelessWidget {
-  const OnBoardingPage({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.subTitle,
-  });
-
-  final String image, title, subTitle;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(MSizes.defaultSpace),
-      child: Column(
-        children: [
-          Image(
-            width: MHelperFunctions.screenWidth() * 0.8,
-            height: MHelperFunctions.screenHeight() * 0.6,
-            image: AssetImage(image),
-          ),
-          Text(
-            title,
-            style: Theme.of(context).textTheme.headlineMedium,
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(height: MSizes.spaceBtwItms),
-          Text(
-            subTitle,
-            style: Theme.of(context).textTheme.bodyMedium,
-            textAlign: TextAlign.center,
-          )
+          const OnBoardingNextButton()
         ],
       ),
     );
