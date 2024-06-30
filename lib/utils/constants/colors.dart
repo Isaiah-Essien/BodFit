d33e0cf639cf267585d860e07ea1779517ffaf39
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-30 13:45:23 -0300

    color.dart

--- lib/utils/constants/colors.dart
+++ lib/utils/constants/colors.dart
@@ -4,8 +4,7 @@ class MColors {
   MColors._();
 
   static const Color primaryColor = Color.fromARGB(255, 29, 144, 215);
-  static const Color dark= Colors.black;
-  static const Color light=Colors.white;
-
-  
+  static const Color dark = Colors.black;
+  static const Color light = Colors.white;
+  static const Color grey = Color(0xff282A2C);
 }
