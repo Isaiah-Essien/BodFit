2df936b956e1d0d5d81db20ac7405be081d099ed
dkokonkwo <d.okonkwo@alustudent.com>
2024-06-30 19:34:34 -0300

    single child

--- lib/features/workout/screens/options/workout_options.dart
+++ lib/features/workout/screens/options/workout_options.dart
@@ -11,56 +11,59 @@ class WorkoutOptions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: GridView.count(
-        crossAxisCount: 2,
-        mainAxisSpacing: 40.0,
-        crossAxisSpacing: 10.0,
-        children: <Widget>[
-          InkWell(
-            onTap: () => Get.to(() => const SubscriptionPage()),
-            child: const CustomImageContainer(
-              imageUrl: MImages.workoutOptions6,
-              imageName: 'Customize',
+    return SingleChildScrollView(
+      child: Center(
+        child: GridView.count(
+          crossAxisCount: 2,
+          mainAxisSpacing: 40.0,
+          crossAxisSpacing: 10.0,
+          children: <Widget>[
+            InkWell(
+              onTap: () => Get.to(() => const SubscriptionPage()),
+              child: const CustomImageContainer(
+                imageUrl: MImages.workoutOptions6,
+                imageName: 'Customize',
+              ),
             ),
-          ),
-          InkWell(
-            onTap: () {
-              Get.to(() => const Hiit());
-            },
-            child: const CustomImageContainer(
-                imageUrl: MImages.workoutOptions2, imageName: 'High Intensity'),
-          ),
-          InkWell(
-            onTap: () {
-              Get.to(() => const WeightLossPlan());
-            },
-            child: const CustomImageContainer(
-                imageUrl: MImages.workoutOptions7, imageName: 'Weight loss'),
-          ),
-          InkWell(
-            onTap: () {
-              Get.to(() => const RecoveryPlan());
-            },
-            child: const CustomImageContainer(
-                imageUrl: MImages.workoutOptions8, imageName: 'Recovery'),
-          ),
-          InkWell(
-            onTap: () {
-              Get.to(() => const CalisthenicsPlan());
-            },
-            child: const CustomImageContainer(
-                imageUrl: MImages.workoutOptions5, imageName: 'Calisthenics'),
-          ),
-          InkWell(
-            onTap: () {
-              Get.to(() => const ThirtyDayPlan());
-            },
-            child: const CustomImageContainer(
-                imageUrl: 'assets/images/opt-1.jpg',
-                imageName: '30-Day Challenge'),
-          ),
-        ],
+            InkWell(
+              onTap: () {
+                Get.to(() => const Hiit());
+              },
+              child: const CustomImageContainer(
+                  imageUrl: MImages.workoutOptions2,
+                  imageName: 'High Intensity'),
+            ),
+            InkWell(
+              onTap: () {
+                Get.to(() => const WeightLossPlan());
+              },
+              child: const CustomImageContainer(
+                  imageUrl: MImages.workoutOptions7, imageName: 'Weight loss'),
+            ),
+            InkWell(
+              onTap: () {
+                Get.to(() => const RecoveryPlan());
+              },
+              child: const CustomImageContainer(
+                  imageUrl: MImages.workoutOptions8, imageName: 'Recovery'),
+            ),
+            InkWell(
+              onTap: () {
+                Get.to(() => const CalisthenicsPlan());
+              },
+              child: const CustomImageContainer(
+                  imageUrl: MImages.workoutOptions5, imageName: 'Calisthenics'),
+            ),
+            InkWell(
+              onTap: () {
+                Get.to(() => const ThirtyDayPlan());
+              },
+              child: const CustomImageContainer(
+                  imageUrl: 'assets/images/opt-1.jpg',
+                  imageName: '30-Day Challenge'),
+            ),
+          ],
+        ),
       ),
     );
   }
