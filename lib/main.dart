498f66800ddb7b309510d5c8fabadbcaf7b91ee7
Isaiah Essien(ub bobo) <i.essien@alustudent.com>
2024-07-06 21:44:34 +0200

    Firebase completely setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,19 @@
+import 'package:bodFit_group5_summative/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'app.dart';
 
-void main() {
+Future<void> main() async {
+
+  //Todo: Add Widgets Binding
+  //Todo: Init Local Storage
+  //Todo: Init Payment method
+  //Todo: Await Native Splash
+  //Todo:Init Firebase
+  // await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform).then(
+  //   (FirebaseApp value)=>Get.put(AuthenticationRepository()),
+  // );
+  //Todo:Init Authentication
   runApp(const App());
 }
